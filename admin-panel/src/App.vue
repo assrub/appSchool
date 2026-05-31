e314bc093000e5e4033d6b45988ed64cd5b85858
rubens <rubdario23@gmail.com>
2026-05-31 22:55:04 +0000

    Fix snackbar state access in App.vue (remove .value)

--- admin-panel/src/App.vue
+++ admin-panel/src/App.vue
@@ -2,22 +2,22 @@
   <v-app>
     <router-view />
     <v-snackbar
-      v-model="snackbar.state.value.show"
-      :color="snackbar.state.value.color"
-      :timeout="snackbar.state.value.timeout"
+      v-model="snackbar.state.show"
+      :color="snackbar.state.color"
+      :timeout="snackbar.state.timeout"
       location="top right"
       rounded="lg"
       elevation="4"
     >
       <div class="d-flex align-center">
-        <v-icon v-if="snackbar.state.value.color === 'error'" class="mr-2" size="20">mdi-alert-circle</v-icon>
-        <v-icon v-else-if="snackbar.state.value.color === 'success'" class="mr-2" size="20">mdi-check-circle</v-icon>
-        <v-icon v-else-if="snackbar.state.value.color === 'warning'" class="mr-2" size="20">mdi-alert</v-icon>
+        <v-icon v-if="snackbar.state.color === 'error'" class="mr-2" size="20">mdi-alert-circle</v-icon>
+        <v-icon v-else-if="snackbar.state.color === 'success'" class="mr-2" size="20">mdi-check-circle</v-icon>
+        <v-icon v-else-if="snackbar.state.color === 'warning'" class="mr-2" size="20">mdi-alert</v-icon>
         <v-icon v-else class="mr-2" size="20">mdi-information</v-icon>
-        <span class="text-body-2">{{ snackbar.state.value.message }}</span>
+        <span class="text-body-2">{{ snackbar.state.message }}</span>
       </div>
       <template #actions>
-        <v-btn icon variant="text" size="small" @click="snackbar.state.value.show = false">
+        <v-btn icon variant="text" size="small" @click="snackbar.state.show = false">
           <v-icon size="18">mdi-close</v-icon>
         </v-btn>
       </template>
