203839172efc2442d8b56165ff83c696f20dbc3d
rubens <rubdario23@gmail.com>
2026-05-31 09:43:27 +0000

    RichTextEditor: tooltips on ALL toolbar buttons
    - Bold, Italic, Underline, Strike: tooltips
    - H1, H2, H3 headings: tooltips
    - Bullet list, numbered list, blockquote: tooltips
    - Color picker, highlight: tooltips
    - Table, image, YouTube, link: tooltips
    - Undo, Redo: tooltips

--- admin-panel/src/components/RichTextEditor.vue
+++ admin-panel/src/components/RichTextEditor.vue
@@ -2,43 +2,39 @@
   <div class="editor-container" v-if="editor">
     <div class="toolbar">
       <v-btn-group density="compact" variant="text">
-        <v-btn icon="mdi-format-bold" size="small" :color="editor.isActive('bold')?'primary':''" @click="editor.chain().focus().toggleBold().run()" />
-        <v-btn icon="mdi-format-italic" size="small" :color="editor.isActive('italic')?'primary':''" @click="editor.chain().focus().toggleItalic().run()" />
-        <v-btn icon="mdi-format-underline" size="small" :color="editor.isActive('underline')?'primary':''" @click="editor.chain().focus().toggleUnderline().run()" />
-        <v-btn icon="mdi-format-strikethrough-variant" size="small" :color="editor.isActive('strike')?'primary':''" @click="editor.chain().focus().toggleStrike().run()" />
+        <v-tooltip text="Negrita (Ctrl+B)" location="bottom"><template #activator="{ props: tp }"><v-btn icon="mdi-format-bold" v-bind="tp" size="small" :color="editor.isActive('bold')?'primary':''" @click="editor.chain().focus().toggleBold().run()" /></template></v-tooltip>
+        <v-tooltip text="Cursiva (Ctrl+I)" location="bottom"><template #activator="{ props: tp }"><v-btn icon="mdi-format-italic" v-bind="tp" size="small" :color="editor.isActive('italic')?'primary':''" @click="editor.chain().focus().toggleItalic().run()" /></template></v-tooltip>
+        <v-tooltip text="Subrayado (Ctrl+U)" location="bottom"><template #activator="{ props: tp }"><v-btn icon="mdi-format-underline" v-bind="tp" size="small" :color="editor.isActive('underline')?'primary':''" @click="editor.chain().focus().toggleUnderline().run()" /></template></v-tooltip>
+        <v-tooltip text="Tachado" location="bottom"><template #activator="{ props: tp }"><v-btn icon="mdi-format-strikethrough-variant" v-bind="tp" size="small" :color="editor.isActive('strike')?'primary':''" @click="editor.chain().focus().toggleStrike().run()" /></template></v-tooltip>
       </v-btn-group>
       <v-divider vertical class="mx-1" />
       <v-btn-group density="compact" variant="text">
-        <v-btn icon="mdi-format-header-1" size="small" :color="editor.isActive('heading',{level:1})?'primary':''" @click="editor.chain().focus().toggleHeading({level:1}).run()" />
-        <v-btn icon="mdi-format-header-2" size="small" :color="editor.isActive('heading',{level:2})?'primary':''" @click="editor.chain().focus().toggleHeading({level:2}).run()" />
-        <v-btn icon="mdi-format-header-3" size="small" :color="editor.isActive('heading',{level:3})?'primary':''" @click="editor.chain().focus().toggleHeading({level:3}).run()" />
+        <v-tooltip text="Título grande" location="bottom"><template #activator="{ props: tp }"><v-btn icon="mdi-format-header-1" v-bind="tp" size="small" :color="editor.isActive('heading',{level:1})?'primary':''" @click="editor.chain().focus().toggleHeading({level:1}).run()" /></template></v-tooltip>
+        <v-tooltip text="Título mediano" location="bottom"><template #activator="{ props: tp }"><v-btn icon="mdi-format-header-2" v-bind="tp" size="small" :color="editor.isActive('heading',{level:2})?'primary':''" @click="editor.chain().focus().toggleHeading({level:2}).run()" /></template></v-tooltip>
+        <v-tooltip text="Título chico" location="bottom"><template #activator="{ props: tp }"><v-btn icon="mdi-format-header-3" v-bind="tp" size="small" :color="editor.isActive('heading',{level:3})?'primary':''" @click="editor.chain().focus().toggleHeading({level:3}).run()" /></template></v-tooltip>
       </v-btn-group>
       <v-divider vertical class="mx-1" />
       <v-btn-group density="compact" variant="text">
-        <v-btn icon="mdi-format-list-bulleted" size="small" :color="editor.isActive('bulletList')?'primary':''" @click="editor.chain().focus().toggleBulletList().run()" />
-        <v-btn icon="mdi-format-list-numbered" size="small" :color="editor.isActive('orderedList')?'primary':''" @click="editor.chain().focus().toggleOrderedList().run()" />
-        <v-btn icon="mdi-format-quote-close" size="small" :color="editor.isActive('blockquote')?'primary':''" @click="editor.chain().focus().toggleBlockquote().run()" />
-        <v-btn icon="mdi-code-tags" size="small" :color="editor.isActive('codeBlock')?'primary':''" @click="editor.chain().focus().toggleCodeBlock().run()" />
+        <v-tooltip text="Lista con viñetas" location="bottom"><template #activator="{ props: tp }"><v-btn icon="mdi-format-list-bulleted" v-bind="tp" size="small" :color="editor.isActive('bulletList')?'primary':''" @click="editor.chain().focus().toggleBulletList().run()" /></template></v-tooltip>
+        <v-tooltip text="Lista numerada" location="bottom"><template #activator="{ props: tp }"><v-btn icon="mdi-format-list-numbered" v-bind="tp" size="small" :color="editor.isActive('orderedList')?'primary':''" @click="editor.chain().focus().toggleOrderedList().run()" /></template></v-tooltip>
+        <v-tooltip text="Cita textual" location="bottom"><template #activator="{ props: tp }"><v-btn icon="mdi-format-quote-close" v-bind="tp" size="small" :color="editor.isActive('blockquote')?'primary':''" @click="editor.chain().focus().toggleBlockquote().run()" /></template></v-tooltip>
       </v-btn-group>
       <v-divider vertical class="mx-1" />
       <v-btn-group density="compact" variant="text">
-        <v-menu :close-on-content-click="false">
-          <template #activator="{ props }"><v-btn icon="mdi-palette" size="small" v-bind="props" /></template>
-          <v-color-picker v-model="selectedColor" @update:model-value="setColor" hide-inputs width="200" />
-        </v-menu>
-        <v-btn icon="mdi-highlighter" size="small" :color="editor.isActive('highlight')?'primary':''" @click="editor.chain().focus().toggleHighlight().run()" />
+        <v-tooltip text="Color de texto" location="bottom"><template #activator="{ props: tp }"><v-menu :close-on-content-click="false"><template #activator="{ props }"><v-btn icon="mdi-palette" size="small" v-bind="{...tp,...props}" /></template><v-color-picker v-model="selectedColor" @update:model-value="setColor" hide-inputs width="200" /></v-menu></template></v-tooltip>
+        <v-tooltip text="Resaltar texto" location="bottom"><template #activator="{ props: tp }"><v-btn icon="mdi-highlighter" v-bind="tp" size="small" :color="editor.isActive('highlight')?'primary':''" @click="editor.chain().focus().toggleHighlight().run()" /></template></v-tooltip>
       </v-btn-group>
       <v-divider vertical class="mx-1" />
       <v-btn-group density="compact" variant="text">
-        <v-btn icon="mdi-table-plus" size="small" @click="insertTable" />
-        <v-btn icon="mdi-image" size="small" @click="insertImage" />
-        <v-btn icon="mdi-youtube" size="small" @click="insertVideo" />
-        <v-btn icon="mdi-link" size="small" :color="editor.isActive('link')?'primary':''" @click="toggleLink" />
+        <v-tooltip text="Insertar tabla 3x3" location="bottom"><template #activator="{ props: tp }"><v-btn icon="mdi-table-plus" v-bind="tp" size="small" @click="insertTable" /></template></v-tooltip>
+        <v-tooltip text="Insertar imagen" location="bottom"><template #activator="{ props: tp }"><v-btn icon="mdi-image" v-bind="tp" size="small" @click="insertImage" /></template></v-tooltip>
+        <v-tooltip text="Insertar video de YouTube" location="bottom"><template #activator="{ props: tp }"><v-btn icon="mdi-youtube" v-bind="tp" size="small" @click="insertVideo" /></template></v-tooltip>
+        <v-tooltip text="Insertar link" location="bottom"><template #activator="{ props: tp }"><v-btn icon="mdi-link" v-bind="tp" size="small" :color="editor.isActive('link')?'primary':''" @click="toggleLink" /></template></v-tooltip>
       </v-btn-group>
       <v-divider vertical class="mx-1" />
       <v-btn-group density="compact" variant="text">
-        <v-btn icon="mdi-undo" size="small" @click="editor.chain().focus().undo().run()" />
-        <v-btn icon="mdi-redo" size="small" @click="editor.chain().focus().redo().run()" />
+        <v-tooltip text="Deshacer (Ctrl+Z)" location="bottom"><template #activator="{ props: tp }"><v-btn icon="mdi-undo" v-bind="tp" size="small" @click="editor.chain().focus().undo().run()" /></template></v-tooltip>
+        <v-tooltip text="Rehacer (Ctrl+Y)" location="bottom"><template #activator="{ props: tp }"><v-btn icon="mdi-redo" v-bind="tp" size="small" @click="editor.chain().focus().redo().run()" /></template></v-tooltip>
       </v-btn-group>
     </div>
     <editor-content :editor="editor" class="editor-content" />
